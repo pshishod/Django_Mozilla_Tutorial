b055cd41a9665d7e04ccf54050016af241bd6d4a
pshishod <33633060+pshishod@users.noreply.github.com>
2022-02-13 17:20:18 -0600

    Added password reset and auth

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.urls import reverse
 import uuid
+from django.contrib.auth.models import User
+from datetime import date
+
 
 # Create your models here.
 
@@ -38,6 +41,7 @@ class BookInstance(models.Model):
 	book = models.ForeignKey(Book, on_delete=models.RESTRICT, null=True)
 	imprint = models.CharField(max_length=200)
 	due_back = models.DateField(null=True, blank=True)
+	borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
 	LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -53,6 +57,12 @@ class BookInstance(models.Model):
 	
 	def __str__(self):
 		return f'{self.id} ({self.book.title})'
+
+	@property
+	def is_overdue(self):
+		if self.due_back and date.today() > self.due_back:
+			return True
+		return False
 
 class Author(models.Model):
 	first_name = models.CharField(max_length=100)
